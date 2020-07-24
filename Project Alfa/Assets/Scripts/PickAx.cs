bca13fb603b45bc0ef73117f9a4e24c93578718b
Xavier Rouyer <xrouyer.pro@gmail.com>
2020-07-24 17:37:26 -0400

    PickAxe Script Edit
    Added conditions + initiated functions for each conditions

---
+++ Project Alfa/Assets/Scripts/PickAx.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PickAx : MonoBehaviour
+{
+    private Rigidbody pickAx;
+    private bool isPickAxInHand;
+    void Start()
+    {
+        pickAx = GameObject.FindGameObjectWithTag("PickAx").gameObject.GetComponent<Rigidbody>();
+        Debug.Log(pickAx.gameObject);
+        isPickAxInHand = true;
+    }
+
+    void Update()
+    {
+        bool isAiming = false;
+
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            if (isPickAxInHand)
+            {
+                isAiming = true;
+                Debug.Log("Aim");
+            }
+            else
+            {
+                PickAxComeBack();
+            }
+            Debug.Log(isPickAxInHand);
+        }
+        else
+        {
+            isAiming = false;
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            if (isPickAxInHand)
+            {
+                if (!isAiming)
+                {
+                    PickAxHit();
+                }
+                else
+                {
+                    PickAxThrow();
+                }
+            }
+            else
+            {
+                PickAxEffect();
+            }
+            Debug.Log(isPickAxInHand);
+        }
+    }
+
+    private void PickAxHit()
+    {
+        Debug.Log("Hit");
+        //Jouer Animation, définir ce qui est touché par la hache, et lancer la fonction qui contrôle la réaction
+    }
+
+    private void PickAxThrow()
+    {
+        Debug.Log("Throw");
+        isPickAxInHand = false;
+    }
+
+    private void PickAxComeBack()
+    {
+        isPickAxInHand = true;
+    }
+
+    private void PickAxEffect()
+    {
+
+    }
+}
